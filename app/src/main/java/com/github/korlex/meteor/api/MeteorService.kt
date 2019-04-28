f8020d2452736fc6e3709d5a08cf0aac8ae68909
korlex <Korchinskii.Alexandr@yandex.ru>
2019-04-28 22:28:16 +0300

    Units format fixes

--- app/src/main/java/com/github/korlex/meteor/api/MeteorService.kt
+++ app/src/main/java/com/github/korlex/meteor/api/MeteorService.kt
@@ -1,6 +1,7 @@
 package com.github.korlex.meteor.api
 
 import com.github.korlex.meteor.api.MeteorApi.API_KEY_QUERY
+import com.github.korlex.meteor.api.MeteorApi.UNITS_METRIC
 import com.github.korlex.meteor.api.model.location.LocationResponse
 import com.github.korlex.meteor.api.model.weather.WeatherResponse
 import io.reactivex.Single
@@ -9,10 +10,10 @@ import retrofit2.http.Query
 
 interface MeteorService {
 
-    @GET("data/2.5/find?$API_KEY_QUERY")
+    @GET("data/2.5/find?$API_KEY_QUERY&$UNITS_METRIC")
     fun getLocations(@Query("q") loc: String?): Single<LocationResponse>
 
-    @GET("data/2.5/forecast?$API_KEY_QUERY")
+    @GET("data/2.5/forecast?$API_KEY_QUERY&$UNITS_METRIC")
     fun getWeather(
         @Query("lat") lat: String,
         @Query("lon") lon: String): Single<WeatherResponse>
